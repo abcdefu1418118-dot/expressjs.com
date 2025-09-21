e8c39926e4b188e08380c610ecc49bf4b5d0a0a0
abcdefu1418118-dot <abcdefu.1418.118@gmail.com>
2025-09-21 04:37:31 -0700

    chore(scripts): harden get-readmes.sh

--- .github/scripts/get-readmes.sh
+++ .github/scripts/get-readmes.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
 
 # The following is a 3 column list of org, repo, and branch.
 # - If the branch is NOT specified, then the README for that project
@@ -7,7 +8,11 @@
 # - If the branch IS specified, it will be used to fetch the README.md
 #   from the given github repo. If that branch is NOT master, then the
 #   branch name will be appended to the local readme file name.
-(cat <<LIST_END
+
+DEST_DIR="../../_includes/readmes"
+mkdir -p "$DEST_DIR"
+
+(cat <<'LIST_END'
 expressjs body-parser master
 expressjs compression master
 expressjs connect-rid master
@@ -27,24 +32,29 @@ expressjs timeout master
 expressjs vhost master
 expressjs express master/examples
 LIST_END
-) | while read org repo branch; do
+) | while read -r org repo branch; do
   # Write the README.md to a file named after the repo
-  DEST="../../_includes/readmes/$repo.md"
+  DEST="$DEST_DIR/$repo.md"
   # When fetching from a branch of a gh repo
   GHURL="https://raw.githubusercontent.com/$org/$repo/$branch/README.md"
   # When fetching from the latest release of a node module
   NPMURL="https://registry.npmjs.org/$repo"
-  if [ -z "$branch" ]; then
+
+  if [ -z "${branch:-}" ]; then
     # No branch means latest release, so fetch from npmjs.org
     echo "fetching $org/$repo from latest npmjs.org release..."
-    curl -s $NPMURL | jq -r '.readme|rtrimstr("\n")' > $DEST
+    curl -fsS "$NPMURL" | jq -r '.readme' | sed -e 's/[[:space:]]*$//' > "$DEST"
   else
     # This allows us to specify a branch other than master if we want to.
     # In this case, the branch name is added to the readme name in the filename.
     if [ "$branch" != "master" ]; then
-      DEST="../../_includes/readmes/$repo-$branch.md"
+      # sanitize the branch string for filename use
+      safe_branch="${branch//\\//-}"
+      DEST="$DEST_DIR/$repo-$safe_branch.md"
     fi
     echo "fetching $org/$repo/$branch from GitHub's raw content domain..."
-    curl -s $GHURL > $DEST
+    if ! curl -fsS "$GHURL" -o "$DEST"; then
+      echo "Failed to fetch $GHURL" >&2
+    fi
   fi
 done
